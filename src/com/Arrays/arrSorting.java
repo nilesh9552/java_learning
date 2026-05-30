a8f6e304811f489637210c867f295713be1ab120
nilesh9552 <njagtap872@gmail.com>
2026-05-30 10:54:28 +0530

    Ignore bin and class files

--- src/com/Arrays/arrSorting.java
+++ src/com/Arrays/arrSorting.java
@@ -10,7 +10,7 @@ public static void main(String[] args) {
 	for(int i = 0 ; i<arr.length; i++ ) {
 		arr[i] = sc.nextInt();	
 		}
-	System.out.println("=========================");
+	System.out.println("=========================+++");
 	
 	
 	Arrays.sort(arr);
